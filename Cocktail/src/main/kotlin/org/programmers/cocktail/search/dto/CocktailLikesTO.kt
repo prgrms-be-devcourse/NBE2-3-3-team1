393451d07becfcfa95a43f600648109c493c5f75
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-23 20:00:35 +0900

    feat: 외부에서 CocktailLikesTO에 데이터를 담을 수 있도록 해당 클래스를 data class로 정의하고, 프로퍼티를 변경 가능한(var) 속성으로 설정

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CocktailLikesTO.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CocktailLikesTO.kt
@@ -2,13 +2,8 @@ package org.programmers.cocktail.search.dto
 
 import lombok.*
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-class CocktailLikesTO {
-    private val id: Long? = null
-    private val userId: Long? = null
-    private val cocktailId: Long? = null
-}
+data class CocktailLikesTO (
+    var id: Long? = null,
+    var userId: Long? = null,
+    var cocktailId: Long? = null,
+)
