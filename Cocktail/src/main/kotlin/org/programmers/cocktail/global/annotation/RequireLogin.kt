baf58be9eb9d8accf3810ca054ab3e6dae132ab8
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-23 15:56:07 +0900

    feat: RequireLogin 어노테이션 클래스 코틀린으로 마이그레이션 진행

---
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/global/annotation/RequireLogin.kt
@@ -0,0 +1,11 @@
+package org.programmers.cocktail.global.annotation
+
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(
+    AnnotationRetention.RUNTIME
+)
+annotation class RequireLogin 
