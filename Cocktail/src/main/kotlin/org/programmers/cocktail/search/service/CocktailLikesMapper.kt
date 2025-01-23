fc477a237f990baafd38aa47e360abde60771213
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-23 17:01:15 +0900

    feat: CocktailLikesMapper 클래스 코틀린 인텔리제이 마이그레이션

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/service/CocktailLikesMapper.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/service/CocktailLikesMapper.kt
@@ -1,44 +1,39 @@
-package org.programmers.cocktail.search.service;
+package org.programmers.cocktail.search.service
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.programmers.cocktail.entity.CocktailLikes;
-import org.programmers.cocktail.search.dto.CocktailLikesTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper
+import org.modelmapper.PropertyMap
+import org.programmers.cocktail.entity.CocktailLikes
+import org.programmers.cocktail.search.dto.CocktailLikesTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
 @Service
-public class CocktailLikesMapper {
-    private ModelMapper modelMapper;
-
-    @Autowired
-    public CocktailLikesMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-
+class CocktailLikesMapper @Autowired constructor(private val modelMapper: ModelMapper) {
+    init {
         // CocktailLikes -> CocktailLikesTO 변환 매핑
-        modelMapper.addMappings(new PropertyMap<CocktailLikes, CocktailLikesTO>() {
-            @Override
-            protected void configure() {
-                map().setUserId(source.getUsers().getId());
-                map().setCocktailId(source.getCocktails().getId());
+        modelMapper.addMappings<CocktailLikes, CocktailLikesTO>(object :
+            PropertyMap<CocktailLikes?, CocktailLikesTO?>() {
+            override fun configure() {
+                map().setUserId(source.getUsers().getId())
+                map().setCocktailId(source.getCocktails().getId())
             }
-        });
+        })
 
         // CocktailLikesTO -> CocktailLikes 변환 매핑
-        modelMapper.addMappings(new PropertyMap<CocktailLikesTO, CocktailLikes>() {
-            @Override
-            protected void configure() {
-                map().getUsers().setId(source.getUserId());
-                map().getCocktails().setId(source.getCocktailId());
+        modelMapper.addMappings<CocktailLikesTO, CocktailLikes>(object :
+            PropertyMap<CocktailLikesTO?, CocktailLikes?>() {
+            override fun configure() {
+                map().getUsers().setId(source.getUserId())
+                map().getCocktails().setId(source.getCocktailId())
             }
-        });
+        })
     }
 
-    public CocktailLikesTO convertToCocktailsLikesTO(CocktailLikes cocktailLikes) {
-        return modelMapper.map(cocktailLikes, CocktailLikesTO.class);
+    fun convertToCocktailsLikesTO(cocktailLikes: CocktailLikes?): CocktailLikesTO {
+        return modelMapper.map(cocktailLikes, CocktailLikesTO::class.java)
     }
 
-    public CocktailLikes convertToCocktailLikes(CocktailLikesTO cocktailLikesTO){
-        return modelMapper.map(cocktailLikesTO, CocktailLikes.class);
+    fun convertToCocktailLikes(cocktailLikesTO: CocktailLikesTO?): CocktailLikes {
+        return modelMapper.map(cocktailLikesTO, CocktailLikes::class.java)
     }
 }
