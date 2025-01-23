9673565a9167e59252aa4514baa01682d8be545f
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-23 17:00:01 +0900

    feat: CocktailListsMapper 클래스 코틀린 인텔리제이 마이그레이션

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/service/CocktailListsMapper.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/service/CocktailListsMapper.kt
@@ -1,46 +1,39 @@
-package org.programmers.cocktail.search.service;
+package org.programmers.cocktail.search.service
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.programmers.cocktail.entity.CocktailLists;
-import org.programmers.cocktail.search.dto.CocktailListsTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper
+import org.modelmapper.PropertyMap
+import org.programmers.cocktail.entity.CocktailLists
+import org.programmers.cocktail.search.dto.CocktailListsTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
 @Service
-public class CocktailListsMapper {
-
-    private ModelMapper modelMapper;
-
-    @Autowired
-    public CocktailListsMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-
+class CocktailListsMapper @Autowired constructor(private val modelMapper: ModelMapper) {
+    init {
         // CocktailLists -> CocktailListsTO 변환 매핑
-        modelMapper.addMappings(new PropertyMap<CocktailLists, CocktailListsTO>() {
-            @Override
-            protected void configure() {
-                map().setUserId(source.getUsers().getId());
-                map().setCocktailId(source.getCocktails().getId());
+        modelMapper.addMappings<CocktailLists, CocktailListsTO>(object :
+            PropertyMap<CocktailLists?, CocktailListsTO?>() {
+            override fun configure() {
+                map().setUserId(source.getUsers().getId())
+                map().setCocktailId(source.getCocktails().getId())
             }
-        });
+        })
 
         // CocktailListsTO -> CocktailLists 변환 매핑
-        modelMapper.addMappings(new PropertyMap<CocktailListsTO, CocktailLists>() {
-            @Override
-            protected void configure() {
-                map().getUsers().setId(source.getUserId());
-                map().getCocktails().setId(source.getCocktailId());
+        modelMapper.addMappings<CocktailListsTO, CocktailLists>(object :
+            PropertyMap<CocktailListsTO?, CocktailLists?>() {
+            override fun configure() {
+                map().getUsers().setId(source.getUserId())
+                map().getCocktails().setId(source.getCocktailId())
             }
-        });
+        })
     }
 
-    public CocktailListsTO convertToCocktailsListsTO(CocktailLists cocktailLists) {
-        return modelMapper.map(cocktailLists, CocktailListsTO.class);
+    fun convertToCocktailsListsTO(cocktailLists: CocktailLists?): CocktailListsTO {
+        return modelMapper.map(cocktailLists, CocktailListsTO::class.java)
     }
 
-    public CocktailLists convertToCocktailLists(CocktailListsTO cocktailListsTO){
-        return modelMapper.map(cocktailListsTO, CocktailLists.class);
+    fun convertToCocktailLists(cocktailListsTO: CocktailListsTO?): CocktailLists {
+        return modelMapper.map(cocktailListsTO, CocktailLists::class.java)
     }
-
 }
