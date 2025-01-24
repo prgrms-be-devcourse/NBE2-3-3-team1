971e40e403791ad0f34475a9bf31b72862968310
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-24 12:34:41 +0900

    feat: Entity 클래스 Kotlin - JPA 호환성 고려하여 변수 선언 설정 변경
    Entity 클래스 프로퍼티에 대해 getter 또는 setter로 접근할 수 있도록 변경

--- Cocktail/src/main/kotlin/org/programmers/cocktail/entity/Users.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/entity/Users.kt
@@ -1,57 +1,62 @@
 package org.programmers.cocktail.entity
 
 import jakarta.persistence.*
-import lombok.*
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 
 @Entity(name = "users")
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Getter
-@Setter
-class Users @Builder constructor(
-    @field:Column(
-        nullable = false,
-        length = 30,
-        unique = true
-    ) private var email: String,
-    @field:Column(
-        nullable = false,
-        length = 20
-    ) private var name: String,
-    @field:Column(nullable = false) private var password: String,
-    @field:Column(
-        nullable = false
-    ) private var gender: String,
-    @field:Column(nullable = false) private var age: Int
+class Users(
+    @Column(nullable = false, length = 30, unique = true)
+    var email: String, // 불변
+
+    @Column(nullable = false, length = 20)
+    var name: String, // 수정 가능
+
+    @Column(nullable = false)
+    var password: String, // 수정 가능
+
+    @Column(nullable = false)
+    var gender: String, // 수정 가능
+
+    @Column(nullable = false)
+    var age: Int // 수정 가능
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val id: Long? = null
+    var id: Long? = null // 불변
 
     @OneToMany(mappedBy = "users", cascade = [CascadeType.ALL], orphanRemoval = true)
-    private val authorities: MutableList<Authorities> = ArrayList()
+    var authorities: MutableList<Authorities> = ArrayList()
+        protected set
 
     @OneToMany(mappedBy = "users", cascade = [CascadeType.ALL], orphanRemoval = true)
-    private val comments: List<Comments> = ArrayList()
+    var comments: MutableList<Comments> = ArrayList()
+        protected set
 
     @OneToMany(mappedBy = "users", cascade = [CascadeType.ALL], orphanRemoval = true)
-    private val cocktailLists: List<CocktailLists> = ArrayList()
+    var cocktailLists: MutableList<CocktailLists> = ArrayList()
+        protected set
 
     @OneToMany(mappedBy = "users", cascade = [CascadeType.ALL], orphanRemoval = true)
-    private val cocktailLikes: List<CocktailLikes> = ArrayList()
-
-    fun updateAuthorities(authorities: Authorities) {
-        this.authorities.add(authorities)
-    }
+    var cocktailLikes: MutableList<CocktailLikes> = ArrayList()
+        protected set
 
     @CreationTimestamp
     @Column(updatable = false)
-    private var createdAt: LocalDateTime? = null
+    var createdAt: LocalDateTime? = null
+        protected set
 
     @UpdateTimestamp
-    private val updatedAt: LocalDateTime? = null
+    var updatedAt: LocalDateTime? = null
+        protected set
+
+    // JPA에서 필요한 기본 생성자 추가
+    constructor() : this(
+        email = "",
+        name = "",
+        password = "",
+        gender = "",
+        age = 0
+    )
 }
