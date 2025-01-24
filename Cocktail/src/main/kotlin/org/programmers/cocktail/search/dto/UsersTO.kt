a3bc01a58c91a6b0482e11da9fde35d99067289e
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-24 13:27:11 +0900

    fix: UsersTO 객체의 프로퍼티에 대해 getter로 데이터를 가져올 수 있도록 data 클래스로 변경

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/UsersTO.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/UsersTO.kt
@@ -2,14 +2,9 @@ package org.programmers.cocktail.search.dto
 
 import lombok.*
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-class UsersTO {
-    private val id: Long? = null
-    private val name: String? = null
-    private val email: String? = null
-    private val password: String? = null
-}
+data class UsersTO (
+    val id: Long? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+)
