e4211a05393edb91fcdad96c53b323876b37458c
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-24 14:46:09 +0900

    fix: CommentsTO 객체의 프로퍼티에 대해 getter, setter를 통해 property에 접근할 수 있도록 data 클래스로 변경

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CommentsTO.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CommentsTO.kt
@@ -3,17 +3,12 @@ package org.programmers.cocktail.search.dto
 import lombok.*
 import java.time.LocalDateTime
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-class CommentsTO {
-    private val id: Long? = null
-    private val content: String? = null
-    private val userId: Long? = null
-    private val userName: String? = null
-    private val cocktailId: Long? = null
-    private val createdAt: LocalDateTime? = null
-    private val updatedAt: LocalDateTime? = null
-}
+data class CommentsTO (
+    var id: Long? = null,
+    var content: String? = null,
+    var userId: Long? = null,
+    var userName: String? = null,
+    var cocktailId: Long? = null,
+    var createdAt: LocalDateTime? = null,
+    var updatedAt: LocalDateTime? = null
+)
