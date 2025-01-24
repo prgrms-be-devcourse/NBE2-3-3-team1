5319f631128e58eb2cb9b4387cbcf45e6ec87311
jaehyunpyun <pyunjaehyun@gmail.com>
2025-01-24 13:17:26 +0900

    fix: CocktailLikesMapper를 통해 CocktailLikes를 CocktailLikesTO로 변환시 CocktailLikesTO 프로퍼티에 set이 가능하도록 변경
    CocktailLikesTO의 userId, cocktailId 를 val에서 var로 변경

--- Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CocktailLikesTO.kt
+++ Cocktail/src/main/kotlin/org/programmers/cocktail/search/dto/CocktailLikesTO.kt
@@ -3,7 +3,7 @@ package org.programmers.cocktail.search.dto
 import lombok.*
 
 data class CocktailLikesTO (
-    var id: Long? = null,
+    val id: Long? = null,
     var userId: Long? = null,
     var cocktailId: Long? = null,
 )
